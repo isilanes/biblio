c68620908b374cd892cd4b751562b9e179db38e5
Iñaki Silanes <isilanes@gmail.com>
2020-08-03 13:34:51 +0200

    Add first working tests

--- biblio/settings.py
+++ biblio/settings.py
@@ -43,7 +43,6 @@ if EXTRA_APPS:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
