9cbccdccba1a2db958ef22a3a1b1d76a2b18fe6e
Iñaki Silanes <isilanes@gmail.com>
2021-09-11 17:06:36 +0200

    Start implementing inline page update.

--- apps/books/static/books/js/books.js
+++ apps/books/static/books/js/books.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+    slider = document.getElementById("slider");
+    output = document.getElementById("pages-count");
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    };
+
+    output.innerHTML = slider.value;
+});
+
 function mytoggle(id) {
     var x = document.getElementById(id);
     if (x.style.display == "none") {
@@ -11,3 +22,9 @@ function toggle_stats() {
     mytoggle('stats-stats-block');
     mytoggle('stats-progress-block');
 };
+
+function toggle_slider() {
+    mytoggle("slider-block");
+    mytoggle("update-button-block");
+    mytoggle("save-button-block");
+}
