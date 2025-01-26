ad8fe55cbe3e9209dddfd514f572c162222006d5
Iñaki Silanes <isilanes@gmail.com>
2025-01-26 12:42:43 +0100

    [BIB-37] Make collapsable top progress bar visually appealing

--- apps/books/templatetags/books_filters.py
+++ apps/books/templatetags/books_filters.py
@@ -7,3 +7,16 @@ register = template.Library()
 @register.filter
 def absolute(value):
     return abs(float(value))
+
+@register.filter
+def wrt_fifty(value: float) -> float:
+    """
+    Given a 'value' "deviation" from 50%, return the final percentage.
+
+    Args:
+        value (float): The deviation from 50%.
+
+    Returns:
+        float: The final percentage value.
+    """
+    return 50 + value/2
